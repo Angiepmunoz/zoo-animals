85c8252e7642ed4d4cc84f057f3da27e0c6b86ea
JackieC1993 <jackiecheung@pursuit.org>
2024-07-15 10:13:38 -0400

    did the coding along with Jordan

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import ZooAnimals from "./components/zooAnimals";
-import Navbar from "./layout/Navbar"
+import ZooAnimals from "./components/ZooAnimals";
+import Navbar from "./layout/Navbar";
 import "./App.css";
 
 function App() {
-  const zooAnimals = ["lion", "giraffe", "elephant","meercat"];
+  const zooAnimals = ["lion", "giraffe", "elephant", "meercat"];
   return (
     <div className="app">
-      <Navbar/>
+      <Navbar />
       <ZooAnimals zooAnimals={zooAnimals} />
     </div>
   );
--- src/components/ZooAnimals.jsx
+++ src/components/ZooAnimals.jsx
@@ -2,15 +2,15 @@
 
 // create a zooAnimals component
 const ZooAnimals = ({zooAnimals}) => {
-    console.log(zooAnimals)
+    // console.log(zooAnimals)
     return (
         <div className="animal-list">
         <div className="animal-list_title">
           <h3>Zoo Animals</h3>
           <div className="animal-list_ animals">
-            {/* {zooAnimals.map((animal) => (
+            {zooAnimals.map((animal) => (
               <li key={animal}>{animal}</li>
-            ))} */}
+            ))}
           </div>
         </div>
       </div>
